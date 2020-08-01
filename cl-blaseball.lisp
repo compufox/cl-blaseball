0348fa21cc03f20c26b54fd231a253f57ae22e6e
ava <dev@computerfox.xyz>
2020-08-01 01:31:46 -0400

    uses new plugger :export-accessors option

--- cl-blaseball.lisp
+++ cl-blaseball.lisp
@@ -5,7 +5,8 @@
 (setq plugger:*api-root* "/database/")
 
 (defjsonclass division ()
-  ((id :json-key "_id") name teams))
+  ((id :json-key "_id") name teams)
+  (:export-accessors))
 
 (defjsonclass team ()
   ((id :json-key "_id")
@@ -14,7 +15,8 @@
    main-color nickname secondary-color
    shorthand emoji slogan shame-runs
    total-shames total-shamings season-shames
-   season-shamings championships))
+   season-shamings championships)
+  (:export-accessors))
 
 (defjsonclass player ()
   ((id :json-key "_id")
@@ -25,12 +27,14 @@
    patheticism ruthlessness shakespearianism
    tenaciousness thwackability tragicness
    unthwackability watchfulness pressurization
-   total-fingers soul deceased))
+   total-fingers soul deceased)
+  (:export-accessors))
 
 (defjsonclass event ()
   ((id :json-key "_id")
    (v :json-key "__v")
-   msg))
+   msg)
+  (:export-accessors))
 
 (defplugs "https://blaseball.com"
     ("allDivisions" (:list division))
